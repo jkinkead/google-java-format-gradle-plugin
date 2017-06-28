61c1e1cac38934c56313384b9398f1eda12aab7d
Simon Herter <sim.herter@gmail.com>
2017-06-29 00:31:39 +0200

    Shell script xtracing

--- scripts/travis-integ-tests.sh
+++ scripts/travis-integ-tests.sh
@@ -5,21 +5,11 @@
 
 IFS=',' read -r -a versions_array <<< "$GRADLE_VERSIONS"
 
-echo
-echo
-echo "*********************************************************************************"
-echo "This job runs the integration tests against the following versions of Gradle:"
-joint_versions=$(printf ", %s" "${versions_array[@]}")
-echo "${joint_versions:2}"
-echo "*********************************************************************************"
-echo
-echo
-
 if [[ "$TRAVIS_JDK_VERSION" != "openjdk7" ]]; then
 	jdk_switcher use openjdk7
 fi
 
-./gradlew publishToMavenLocal
+(set -x; ./gradlew publishToMavenLocal)
 
 if [[ "$TRAVIS_JDK_VERSION" != "openjdk7" ]]; then
 	jdk_switcher use $TRAVIS_JDK_VERSION
@@ -27,7 +17,7 @@ fi
 
 
 for element in "${versions_array[@]}"; do
-	true "****************************************************************************"
-	GRADLE_VERSION="$element" ./gradlew integrationTest --exclude-task publishToMavenLocal
-	true "****************************************************************************"
+	echo "*********************************************************************************"
+	(set -x; GRADLE_VERSION="$element" ./gradlew integrationTest --exclude-task publishToMavenLocal)
+	echo "*********************************************************************************"
 done
